005958e93f63dd779cabd709f25a661779222341
aidadenisa <aida.denisa@yahoo.com>
2020-06-03 15:15:05 +0200

    Create table users

--- dao/AuthDAO.js
+++ dao/AuthDAO.js
@@ -1,1 +1,19 @@
 
+var sqlDB;
+
+exports.authDBSetup = function (connection) {
+  sqlDB = connection;
+  console.log('checking if the user table exist');
+  return sqlDB.schema.hasTable('user').then((exists) => {
+    if(!exists) {
+      console.log('CREATE TABLE');
+      return sqlDB.schema.withSchema('public').createTable('user', (table) => {
+        table.increments(); // the user id - PK
+        table.string('username');
+        table.string('hash');
+        table.boolean('admin');
+      })
+    }
+  });
+};
+
