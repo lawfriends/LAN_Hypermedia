a2460c97acb2d7c0049f428fd604c4f01494ac1c
lawfriends <lorenzo.amici@outlook.com>
2020-06-03 19:27:49 +0200

    continued developing INS-34 connection with DB

--- public/assets/js/course.js
+++ public/assets/js/course.js
@@ -13,13 +13,13 @@ function getCourse() {
         console.log(`${description}`);
         document.querySelector(".breadcrumb .active").innerHTML = course.level;
         console.log(document.querySelectorAll("#coursePar h1"));
-        document.querySelector("#coursePar h1").innerHTML = course.level;
+        document.querySelector("#coursePar h1").innerHTML = course.level.concat(" course");
         document.querySelector(".CERFlevelPar").innerHTML = course.cerf_level;
         document.querySelector("#coursePar p.normal").innerHTML = course.description;
         document.querySelector(".courseSchedule .card-header").innerHTML = course.day;
         document.querySelector(".courseSchedule .overline").innerHTML = course.time;
-        document.querySelector(".courseSchedule .card-header").innerHTML = course.day;
-        document.querySelector(".courseSchedule .overline").innerHTML = course.time;
+        /* document.querySelector(".courseSchedule:nth-of-type(2) .card-header").innerHTML = course.day;
+        document.querySelector(".courseSchedule:nth-of-type(2) .overline").innerHTML = course.time; */
     })
 }
 
