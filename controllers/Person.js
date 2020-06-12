fc0d5d291c81ea4458c5c75c2b581f451f03e9c1
aidadenisa <aida.denisa@yahoo.com>
2020-06-12 18:13:33 +0200

    Updated code for Person after code review

--- controllers/Person.js
+++ controllers/Person.js
@@ -17,8 +17,6 @@ module.exports.peopleGET = function peopleGET (req, res, next) {
 
 module.exports.personIdGET = function personIdGET (req, res, next) {
   var id = req.swagger.params['id'].value;
-  var limit = req.swagger.params['limit'].value;
-  var offset = req.swagger.params['offset'].value;
   PersonService.personIdGET(id,limit,offset)
     .then(function (response) {
       utils.writeJson(res, response);
